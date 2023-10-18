8db6b0be48961a2e9ef938ae0d4fb2d3d93dddca
YEONSEO <yeonseochoi0717@gmail.com>
2023-10-18 19:39:08 +0900

    fill the skeleton code of function even_list

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def even_list(int_list: List[int]) -> List[int]:
         A list of even integers.
     """
     # TODO: Implement even_list
+    temp_list = []
+
+    for num1 in int_list:
+        if num1 % 2 == 0:
+            temp_list.append(num1)
+            
+    return temp_list
+
     pass
 
 # Skeleton code for sum_of_squares_of_even
